c36f3f695560b6c1c780e8c8db577907d7b1f36f
AakeemCodes <19845846+4th3LuLz0fIt@users.noreply.github.com>
2022-01-31 03:23:31 -0500

    made some changes corrected a few errors

--- main.py
+++ main.py
@@ -4,23 +4,22 @@ Platformer Game
 import arcade
 
 # Constants
-
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 650
 SCREEN_TITLE = "Platformer"
-# Constant for movement and speed of player
-PLAYER_MOVEMENT_SPEED = 5
 
-# Constants used to scale sprites
+# Constants used to scale our sprites from their original size
 CHARACTER_SCALING = 1
-TITLE_SCALING = 0.5
+TILE_SCALING = 0.5
+COIN_SCALING = 0.5
+SPRITE_PIXEL_SIZE =128
+GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
 
-# Constant for gravity and jump speed
+# Constant for movement and speed of character, pixel per frame
+PLAYER_MOVEMENT_SPEED = 10
 GRAVITY = 1
 PLAYER_JUMP_SPEED = 20
 
-# Add Coins and sound
-COIN_SCALING = 0.5
 
 class MyGame(arcade.Window):
     """
@@ -30,7 +29,10 @@ class MyGame(arcade.Window):
     def __init__(self):
         # Call parent class and setup window
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
-
+        
+        # Tile map object
+        self.tile_map = None
+        
         # Scene Object
         self.scene = None
 
@@ -39,20 +41,19 @@ class MyGame(arcade.Window):
         
         # Camera for scrolling the screen
         self.camera = None
-                
-                
+                       
         # Our physics engine
         self.physics_engine = None
-        
-        # Load sounds
-        self.collect_coin_sound = arcade.load_sound(":resources:sounds/coin1.wav")
-        self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
-        
+         
         # Camera used to draw GUI elements
         self.gui_camera =None
         
         # Score tracking
         self.score = 0
+        
+        # Load sounds
+        self.collect_coin_sound = arcade.load_sound(":resources:sounds/coin1.wav")
+        self.jump_sound = arcade.load_sound(":resources:sounds/jump1.wav")
         
         # Color for background
         arcade.set_background_color(arcade.color.EBONY)
@@ -61,8 +62,27 @@ class MyGame(arcade.Window):
     def setup(self):
         """setup game here. call this function to restart the game"""
 
-        # Seup Camera
+        # Setup Camera
         self.camera = arcade.Camera(self.width, self.height)
+        
+        # Name of map file to load
+        map_name = ":resources:tiled_maps/map.json"
+         
+        # Layer specific options are defined based on Layer names in a dictionary
+        # Doing this will make the SpriteList for the platforms layer
+        # use spatial hashing for detection.
+        layer_options = {
+            "Platforms": {
+                "use_spatial_hash": True,
+            },
+        }
+        
+        # Read the tiled map
+        self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
+        
+        # Initialize Scene with our TileMap, this will automatically add all layers
+        # from the map as SpriteLists in the scene in the proper order.
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
         
         # Initialize Scene
         self.scene = arcade.Scene()
@@ -71,17 +91,30 @@ class MyGame(arcade.Window):
         self.scene.add_sprite_list("Player")
         self.scene.add_sprite_list("Walls", use_spatial_hash=True)
 
+        # keep track of score
+        self.score = 0
+        
         # Set up the player, placing at its coordinates
         image_source = ":resources:images/animated_characters/female_Adventurer/femaleAdventurer_idle.png"
         self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
-        self.player_sprite.center_x = 64
+        self.player_sprite.center_x = 128
         self.player_sprite.center_y = 128
         self.scene.add_sprite("Player", self.player_sprite)
 
+        # --- Other stuff
+        # Set the background color
+        if self.tile_map.background_color:
+            arcade.set_background_color(self.tile_map.background_color)
+        
+        # Create the 'physics engine'
+        self.physics_engine = arcade.PhysicsEnginePlatformer(
+            self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Platforms"]
+        )
+          
         # create the ground
         # shows using loop to place multiple sprites horizontally
         for x in range(0, 1250, 64):
-            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TITLE_SCALING)
+            wall = arcade.Sprite(":resources:images/tiles/grassMid.png", TILE_SCALING)
             wall.center_x = x
             wall.center_y = 32
             self.scene.add_sprite("Walls", wall)
@@ -93,7 +126,7 @@ class MyGame(arcade.Window):
         for coordinate in coordinates_list:
             # add a crate on the ground
             wall = arcade.Sprite(""
-                                 ":resources:images/tiles/boxCrate_double.png", TITLE_SCALING
+                                 ":resources:images/tiles/boxCrate_double.png", TILE_SCALING
                                  )
             wall.position = coordinate
             self.scene.add_sprite("Walls", wall)
